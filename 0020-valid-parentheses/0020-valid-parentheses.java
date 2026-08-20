b3366520e96fbce43758beb33e3157f542f9677e
Hardik B Vanza <hardikvanza15@gmail.com>
2026-08-21 01:02:14 +0530

    Time: 4 ms (36.68%), Space: 43 MB (86.12%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -4,26 +4,23 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
 
-        for (int i = 0; i < s.length(); i++) {
+        for(int i = 0; i < s.length(); i++){
+
             char c = s.charAt(i);
 
-            // Push opening brackets
-            if (c == '(' || c == '{' || c == '[') {
+            if(c == '(' || c == '{' || c == '['){
                 stack.push(c);
-            } else {
-                // Agar stack empty hai -> invalid
-                if (stack.isEmpty())
-                    return false;
+            }
+            else{
+                if(stack.isEmpty()) return false;
 
                 char top = stack.pop();
-                // Matching check
-                if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) {
-                    return false;
-                }
-            }
-        }
 
-        // Agar stack empty hai -> valid
+                if( top == '(' && c != ')' || top == '{' && c != '}' || top == '[' && c != ']') return false;
+            }
+
+            
+        }
         return stack.isEmpty();
     }
 }
