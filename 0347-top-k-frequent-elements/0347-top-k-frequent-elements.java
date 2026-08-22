e53f22feec5f02a7753c3a66cab620d64ff8eed8
Hardik B Vanza <hardikvanza15@gmail.com>
2026-08-22 11:31:01 +0530

    Time: 13 ms (80.17%), Space: 48 MB (13.34%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,26 +1,53 @@
+// class Solution {
+//     public int[] topKFrequent(int[] nums, int k) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+
+//         for (int i : nums) {
+//             map.put(i, map.getOrDefault(i, 0) + 1);
+//         }
+
+//         // Step 2: Min-Heap based on frequency
+//         PriorityQueue<Map.Entry<Integer, Integer>> heap = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+
+//         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+//             heap.add(entry);
+//             if (heap.size() > k) {
+//                 heap.poll(); // remove smallest freq
+//             }
+//         }
+
+//         // Step 3: Collect result
+//         int[] res = new int[k];
+//         int i = 0;
+//         while (!heap.isEmpty()) {
+//             res[i++] = heap.poll().getKey();
+//         }
+//         return res;
+//     }
+// }
+
+//MAX-HEAP
+
+import java.util.*;
+
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int i : nums) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
+        // Step 1: Frequency map
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int n : nums) {
+            freq.put(n, freq.getOrDefault(n, 0) + 1);
         }
 
-        // Step 2: Min-Heap based on frequency
-        PriorityQueue<Map.Entry<Integer, Integer>> heap = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+        // Step 2: Max-Heap based on frequency
+        PriorityQueue<Map.Entry<Integer, Integer>> heap =
+            new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
 
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            heap.add(entry);
-            if (heap.size() > k) {
-                heap.poll(); // remove smallest freq
-            }
-        }
+        heap.addAll(freq.entrySet());
 
-        // Step 3: Collect result
+        // Step 3: Collect top k
         int[] res = new int[k];
-        int i = 0;
-        while (!heap.isEmpty()) {
-            res[i++] = heap.poll().getKey();
+        for (int i = 0; i < k; i++) {
+            res[i] = heap.poll().getKey();
         }
         return res;
     }
