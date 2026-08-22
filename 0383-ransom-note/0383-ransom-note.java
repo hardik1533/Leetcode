bfce1cc19ef15b61a0ceb8441406cf4f1aa4d1f7
Hardik B Vanza <hardikvanza15@gmail.com>
2026-08-22 10:30:32 +0530

    Time: 3 ms (51.42%), Space: 46.2 MB (79.38%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,16 +1,38 @@
+// class Solution {
+//     public boolean canConstruct(String r, String m) {
+//         if(r.length() > m.length()) return false;
+
+//         int[] freq = new int[26];
+
+//         for(int i = 0; i < m.length(); i++){
+//             if(i < r.length()) freq[r.charAt(i) - 'a']++;
+//             freq[m.charAt(i) - 'a']--;
+//         }
+
+//         for(int i : freq){
+//             if(i > 0) return false;
+//         }
+
+//         return true;
+//     }
+// }
+
+//Optimal : 
 class Solution {
     public boolean canConstruct(String r, String m) {
-        if(r.length() > m.length()) return false;
+        if (r.length() > m.length()) return false;
 
         int[] freq = new int[26];
 
-        for(int i = 0; i < m.length(); i++){
-            if(i < r.length()) freq[r.charAt(i) - 'a']++;
-            freq[m.charAt(i) - 'a']--;
+        // Count magazine letters
+        for (char c : m.toCharArray()) {
+            freq[c - 'a']++;
         }
 
-        for(int i : freq){
-            if(i > 0) return false;
+        // Check ransomNote letters
+        for (char c : r.toCharArray()) {
+            freq[c - 'a']--;
+            if (freq[c - 'a'] < 0) return false;
         }
 
         return true;
