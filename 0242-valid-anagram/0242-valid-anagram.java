2a4f237ec25d64f2a097d50537c1955a2e8a5f25
Hardik B Vanza <hardikvanza15@gmail.com>
2026-08-22 10:10:15 +0530

    Time: 5 ms (62.03%), Space: 44.2 MB (82.51%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,18 +1,36 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-        if(s.length() != t.length()) return false;
+        // if(s.length() != t.length()) return false;
 
-        HashMap<Character,Integer> map = new HashMap<>();
+        // HashMap<Character,Integer> map = new HashMap<>();
 
-        for(char ch : s.toCharArray()){
-            map.put(ch, map.getOrDefault(ch,0) + 1);
+        // for(char ch : s.toCharArray()){
+        //     map.put(ch, map.getOrDefault(ch,0) + 1);
+        // }
+        // for(char ch : t.toCharArray()){
+        //     int a = map.getOrDefault(ch,0);
+        //     if(a == 0) return false;
+        //     map.put(ch, a - 1);
+        // }
+        // return true;
+
+        //Optimal : 
+        
+        if (s.length() != t.length()) return false;
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
         }
-        for(char ch : t.toCharArray()){
-            int a = map.getOrDefault(ch,0);
-            if(a == 0) return false;
-            map.put(ch, a - 1);
+
+        for (int c : count) {
+            if (c != 0) return false;
         }
         return true;
     }
 }
+
+    
