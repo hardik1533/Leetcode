6a82a0cab8cafabdce91908a813ccf82381544d2
Hardik B Vanza <hardikvanza15@gmail.com>
2026-08-22 15:32:13 +0530

    Time: 8 ms (33.94%), Space: 49.8 MB (44.64%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,29 +1,18 @@
-import java.util.*;
-
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
 
         for (String s : strs) {
-            // frequency array for 26 lowercase letters
-            int[] count = new int[26];
-            for (char c : s.toCharArray()) {
-                count[c - 'a']++;
-            }
+            // Convert string to char array and sort it
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String sorted = new String(chars);
 
-            // build key from frequency array
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < 26; i++) {
-                if (count[i] > 0) {
-                    sb.append((char)(i + 'a')).append(count[i]);
-                }
-            }
-            String key = sb.toString();
-
-            // group words by key
-            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+            // Add to map
+            map.computeIfAbsent(sorted, k -> new ArrayList<>()).add(s);
         }
 
+        // Return grouped anagrams
         return new ArrayList<>(map.values());
     }
 }
