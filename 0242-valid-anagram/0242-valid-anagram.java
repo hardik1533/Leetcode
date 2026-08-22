d4e970e6afec342a66fd4552d24f64e84d3cdce4
Hardik B Vanza <hardikvanza15@gmail.com>
2026-08-22 10:07:15 +0530

    Time: 12 ms (28.38%), Space: 46 MB (51.07%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()) return false;
+
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        for(char ch : s.toCharArray()){
+            map.put(ch, map.getOrDefault(ch,0) + 1);
+        }
+        for(char ch : t.toCharArray()){
+            int a = map.getOrDefault(ch,0);
+            if(a == 0) return false;
+            map.put(ch, a - 1);
+        }
+        return true;
+    }
+}
