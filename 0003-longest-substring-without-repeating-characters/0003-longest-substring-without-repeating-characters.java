914e9cde7b598908d31eb2bfa7364546ee807ce3
Hardik B Vanza <hardikvanza15@gmail.com>
2026-08-24 11:35:14 +0530

    Time: 68 ms (16.3%), Space: 47.8 MB (34.03%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,35 +1,52 @@
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         //    Brute :
+//         // Set<Character> set = new HashSet<>();
+//         // int n = s.length();
+//         // int max = 0;
+//         // if(n == 1) return 1;
+//         // for(int i = 0; i < n; i++){
+//         //     for(int j = i; j < n; j++){
+//         //         if(!set.contains(s.charAt(j))){
+//         //             set.add(s.charAt(j));
+//         //         }else{
+//         //             if(set.size() > max) max = set.size();
+//         //             set.clear();
+//         //             break;
+//         //         }
+//         //     }
+//         // }
+//         // return max;
+
+//         // Optimal : 
+//         Set<Character> set = new HashSet<>();
+//         int l = 0;
+//         int maxL = 0;
+//         for (int r = 0; r < s.length(); r++) {
+//             while (set.contains(s.charAt(r))) {
+//                 set.remove(s.charAt(l));
+//                 l++;
+//             }
+//             set.add(s.charAt(r));
+//             maxL = ((r - l + 1) > maxL) ? (r - l + 1) : maxL;
+//         }
+//         return maxL;
+//     }
+// }
+
+//Optimal : 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-    //    Brute :
-        // Set<Character> set = new HashSet<>();
-        // int n = s.length();
-        // int max = 0;
-        // if(n == 1) return 1;
-        // for(int i = 0; i < n; i++){
-        //     for(int j = i; j < n; j++){
-        //         if(!set.contains(s.charAt(j))){
-        //             set.add(s.charAt(j));
-        //         }else{
-        //             if(set.size() > max) max = set.size();
-        //             set.clear();
-        //             break;
-        //         }
-        //     }
-        // }
-        // return max;
-
-    // Optimal : 
         Set<Character> set = new HashSet<>();
-        int left = 0;
-        int maxLength = 0;
-        for(int right = 0; right < s.length(); right++){
-            while(set.contains(s.charAt(right))){
-                set.remove(s.charAt(left));
-                left++;
+        int l = 0, max = 0;
+
+        for (int r = 0; r < s.length(); r++) {
+            while (set.contains(s.charAt(r))) {
+                set.remove(s.charAt(l++));
             }
-            set.add(s.charAt(right));
-            maxLength = ((right - left + 1) > maxLength) ? (right - left + 1) : maxLength;
+            set.add(s.charAt(r));
+            max = Math.max(max, r - l + 1);
         }
-        return maxLength;
+        return max;
     }
 }
