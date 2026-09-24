c8388fe14ec9d58c5e02bc96cc73c52a7ac5b138
Hardik B Vanza <hardikvanza15@gmail.com>
2026-09-24 15:23:42 +0530

    Time: 4 ms (94.81%), Space: 77.2 MB (68.9%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,7 +1,6 @@
 class Solution {
     public int maxArea(int[] h) {
         int i = 0, j = h.length - 1;
-        int maxLeft = 0, maxRight = 0;
         int ans = 0;
         while(i < j){
             int maxW = (Math.min(h[i],h[j]) * (j - i));
