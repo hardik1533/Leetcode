b60b561b1c237b1a5d45dd576c7eac0b1b7b587c
Hardik B Vanza <hardikvanza15@gmail.com>
2026-09-24 15:14:49 +0530

    Time: 37 ms (22.1%), Space: 59.2 MB (45.37%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,29 +1,31 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int length = nums.length,sum;
         Arrays.sort(nums);
-        List<List<Integer>> list = new ArrayList<>();
-        for(int i = 0; i < length; i++){
-            if(i > 0  && nums[i] == nums[i - 1]) continue;
-           int j = i + 1, k = length - 1;
-                   while(j < k){
-                    sum = nums[i] + nums[j] + nums[k];
-                    if(sum < 0) j++;
-                    else if(sum > 0) k--;
-                    else{
-                        List<Integer> list1 = new ArrayList<>();
-                        list1.add(nums[i]);
-                        list1.add(nums[j]);
-                        list1.add(nums[k]);
-                        list.add(list1);
-                        j++;
-                        k--;
-                        while(j < k && nums[j] == nums[j - 1]) j++;
-                    }
-                   }
-                
-        }
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
 
-        return list;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int j = i + 1, k = n - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return ans;
     }
 }
