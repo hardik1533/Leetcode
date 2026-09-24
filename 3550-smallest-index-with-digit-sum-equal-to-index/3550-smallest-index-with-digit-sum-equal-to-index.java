1bc32e3643481e4b62f718b9a029f40b6ef27108
Hardik B Vanza <hardikvanza15@gmail.com>
2026-09-24 11:16:44 +0530

    Time: 1 ms (99.69%), Space: 45.5 MB (59.67%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -1,8 +1,7 @@
 class Solution {
     public int smallestIndex(int[] nums) {
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] == i && nums[i] < 10) return i;
-            else if(nums[i] > 9){
+            
                 int sum = 0;
                 int temp = nums[i];
                 while(temp > 0){
@@ -10,7 +9,7 @@ class Solution {
                     temp /= 10;
                 }
                 if(sum == i) return i;
-            }
+            
         }
         return -1;
     }
