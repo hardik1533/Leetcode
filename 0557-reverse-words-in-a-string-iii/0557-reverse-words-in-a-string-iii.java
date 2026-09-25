a3d3913d9f5a96989f3604bcff671c672a528961
Hardik B Vanza <hardikvanza15@gmail.com>
2026-09-25 09:15:19 +0530

    Time: 7 ms (37%), Space: 47.1 MB (18.6%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,23 +1,43 @@
-class Solution {
-    public String reverse(String t){
-        String rev = new StringBuilder(t).reverse().toString();
-        return rev;
-    }
+// class Solution {
+//     public String reverse(String t){
+//         String rev = new StringBuilder(t).reverse().toString();
+//         return rev;
+//     }
 
+//     public String reverseWords(String s) {
+//         StringBuilder sb = new StringBuilder();
+//         int k = 0;
+//         for(int i  = 0; i < s.length(); i++){
+//             if(s.charAt(i) == ' ' ){
+//                 sb.append(reverse(s.substring(k,i)));
+//                 sb.append(' ');
+//                 k = i + 1;
+//             }else if(i == s.length() - 1){
+//                 sb.append(reverse(s.substring(k,i + 1)));
+//                 k = i + 1;
+//             }
+//         }
+
+//         return sb.toString();
+//     }
+// }
+
+
+
+//Optimal : 
+class Solution {
     public String reverseWords(String s) {
         StringBuilder sb = new StringBuilder();
         int k = 0;
-        for(int i  = 0; i < s.length(); i++){
-            if(s.charAt(i) == ' ' ){
-                sb.append(reverse(s.substring(k,i)));
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == ' '){
+                sb.append(new StringBuilder(s.substring(k, i)).reverse());
                 sb.append(' ');
                 k = i + 1;
-            }else if(i == s.length() - 1){
-                sb.append(reverse(s.substring(k,i + 1)));
-                k = i + 1;
+            } else if(i == s.length() - 1){
+                sb.append(new StringBuilder(s.substring(k, i + 1)).reverse());
             }
         }
-
         return sb.toString();
     }
 }
