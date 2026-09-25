0990b379c04bf410ea739b9ed688689d9f669e5d
Hardik B Vanza <hardikvanza15@gmail.com>
2026-09-25 09:20:54 +0530

    Time: 3 ms (99.48%), Space: 46.4 MB (86.63%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -25,19 +25,51 @@
 
 
 //Optimal : 
+// class Solution {
+//     public String reverseWords(String s) {
+//         StringBuilder sb = new StringBuilder();
+//         int k = 0;
+//         for(int i = 0; i < s.length(); i++){
+//             if(s.charAt(i) == ' '){
+//                 sb.append(new StringBuilder(s.substring(k, i)).reverse());
+//                 sb.append(' ');
+//                 k = i + 1;
+//             } else if(i == s.length() - 1){
+//                 sb.append(new StringBuilder(s.substring(k, i + 1)).reverse());
+//             }
+//         }
+//         return sb.toString();
+//     }
+// }
+
+//Using 2 Pointer: 
+
+
 class Solution {
     public String reverseWords(String s) {
-        StringBuilder sb = new StringBuilder();
-        int k = 0;
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == ' '){
-                sb.append(new StringBuilder(s.substring(k, i)).reverse());
-                sb.append(' ');
-                k = i + 1;
-            } else if(i == s.length() - 1){
-                sb.append(new StringBuilder(s.substring(k, i + 1)).reverse());
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        int start = 0;
+
+        for (int end = 0; end < n; end++) {
+            if (arr[end] == ' ') {
+                reverse(arr, start, end - 1);
+                start = end + 1;
             }
         }
-        return sb.toString();
+        // reverse last word
+        reverse(arr, start, n - 1);
+
+        return new String(arr);
+    }
+
+    private void reverse(char[] arr, int i, int j) {
+        while (i < j) {
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
     }
 }
